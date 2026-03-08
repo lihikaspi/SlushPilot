9aa109fa261736e00abc3dc3caf8b854b528caa6
harel.oved <harel.oved@campus.technion.ac.il>
2026-03-08 23:24:13 +0200

    added frontend render support

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://slushpilot-ui.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
